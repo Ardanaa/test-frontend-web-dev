e956b3aa7ef99bb57e22c9eb7afe2f1753023ea0
Ardana <ardana733@gmail.com>
2024-02-25 03:50:31 +0700

    pinia

--- src/main.js
+++ src/main.js
@@ -8,9 +8,12 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { createPinia } from 'pinia'
 
 // globally import tailwind classes
 import './assets/tailwind.css'
+
+export const pinia = createPinia()
 
 const vuetify = createVuetify({
   components,
@@ -18,6 +21,8 @@ const vuetify = createVuetify({
 })
 const app = createApp(App)
 
+
 app.use(router)
+app.use(pinia)
 app.use(vuetify)
 app.mount('#app')
